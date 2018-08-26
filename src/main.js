4b1565ac9391074c73745029ad9338a4c9e0032d
ivonnehg <hgivonne@gmail.com>
2018-08-26 04:12:54 -0500

    subiendo cambios

--- src/main.js
+++ src/main.js
@@ -1,90 +1,98 @@
 
- document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
 });
 
 // Or with jQuery
 $(document).ready(function () {
-    $('.carousel').carousel();
+  $('.carousel').carousel();
 })
 
-//
-const btnEthnic = document.getElementById('ethnic');
-const btnBudget = document.getElementById('budget');
-//const inputdistrict = document.getElementById('search');
-const btnSearch = document.getElementById('btnsearch');
+//iniciar variables dom
+
+const inputType = document.getElementById('search');
+const btnSearch = document.getElementById('btnSearch');
 let cardRest = document.querySelector('.card-columns');
+const contRestaurants = document.getElementById("restaurantes");
 
 
-let districtRest = '';
+let typeRest = '';
 let search = '';
+let resultSearch = '';
+//recargar 
+window.loadData = (buscar) => {
 
-const data = (search) => {
-
-
-  fetch('https://patriciamont.github.io/lim-2018-01-foodmap/data/restaurants.json')
-    .then(response =>
-      response.json())
-    .then(result => {
-
-      cardRest.innerHTML = '';
-
-      const restArray = searchRestaurants(result, search)
-      showRestaurant(restArray)
-      console.log(restArray);
-    })
-
-
-}
-window.onload=()=>{data('')}
-
-showRestaurant = (restArray) => {
-
-
-  restArray.forEach(element => {
-
-    cardRest.innerHTML +=
-      `<div class="card">
-<img class="card-img-top" src="https://patriciamont.github.io/lim-2018-01-foodmap/img/${element.photo}" alt="Card image cap">
-<div class="card-body">
-<h5 class="card-title">${element.name}</h5>
-<div class="container">
-  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Más Info</button>
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h3 class="modal-title">${element.name}</h3>
-        </div>
-        <div class="modal-body">
-          <h6>Dirección: ${element.address}</h6>
-          <h6>Horario: ${element.schedule}</h6>
-          <h6 class="text-justify">Descripción: ${element.description}</h6>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Pedir Ahora</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-</div>
-</div>
-</div>
-`
-  });
-
-}
-
-btnSearch.addEventListener('click', () => {
-
-  search = inputdistrict.value;
-  /* dataRest('') */
-
-  data(search)
-
-
+  firebase.database().ref('/restaurants').on('child_added', function (snapshot) {
+  //resultSearch = crearElementos(name, type.value, address, district, shedule, description, photo)
+  resultSearch = crearElementos("pollo", "polleria", "chosica", "chos", "", "pollito","pollerias/roky.jpg","parrillas");
+  contRestaurants.innerHTML += resultSearch;
 })
 
+
+
+}
+
+btnSearch.addEventListener('click',()=>{
+
+  var buscar = inputType.value;
+  loadData(buscar);
+
+
+
+});
+
+function crearElementos(name, type, address, district, shedule, description, photo) {
+
+  var contRest = "";
+
+  contRest += '<div class="col s12 m4"><div class="card"><div class="card-image">';
+
+  contRest += '<img src="images/' + photo + '" width="150" height="200">';
+
+  contRest += '<span class="card-title">Card Title</span></div><div class="card-content"><p>';
+
+  contRest += '<b>Nombre: </b> <span>' + name + '</span>';
+  contRest += '<br>';
+  contRest += '<b>Nombre: </b> <span>' + district + '</span>';
+  contRest += '<br>';
+  contRest += '<b>Horario: </b> <span>' + shedule + '</span>';
+  contRest += '<br>';
+  contRest += '<b>Descripcion: </b> <span>' + description + '</span>';
+  contRest += '<br>';
+  contRest += '<b>Dirección: </b> <span>' + address + '</span>';
+  contRest += '<br>';
+  contRest += '<b>Categoría: </b> <span>' + type + '</span>';
+
+
+  contRest += '</p></div><div class="card-action"><a href="#">Ver más</a></div></div></div>';
+
+  return contRest;
+
+  /*
+  <div class="col s12 m4">
+                          <div class="card">
+                            <div class="card-image">
+                              <img src="images/pollerias/norkys.jpg" width="150" height="200">
+                              <span class="card-title">Card Title</span>
+                            </div>
+                            <div class="card-content">
+                              <p>
+
+                                <b>Nombre: </b> <span>Norkys</span>
+                                <br>
+                                <b>Dirección: </b> <span>Chosica</span>
+
+                              </p>
+                            </div>
+                            <div class="card-action">
+                              <a href="#">Ver más</a>
+                            </div>
+                          </div>
+
+                        </div>
+  */
+}
+
+//var imagen = snap.photo;
+//<img src="images/ '+ imagen +' " class="sizePicture">
